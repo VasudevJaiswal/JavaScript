dbe73e6f1675b81b8655b6634e87aa67b8cf92b7
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-07-28 21:27:37 +0530

    Scripts.js

---
+++ 04-Functions/Conditional.js
@@ -0,0 +1,28 @@
+  // Conditional in JavaScript 
+        // If else Statement 
+        var age = 18;
+        if (age>=24){
+            console.log('You can drink bear or liquor ' );
+            
+        }
+        else{
+            console.log('You are not eligible for drink bear or liquor  ');
+        }
+
+        if else Ladder 
+        if(age> 35){
+            console.log("You are a kid");
+        }
+        else (age> 35){
+            console.log("Yes you are not a kid ");
+        }
+        else if(age> 22){
+            console.log("Yes you are not a kid ");
+        }
+        else if(age> 26){
+            console.log("Yes you are not a kid ");
+        }
+        else if(age> 24){
+            console.log("Yes you are not a kid ");
+        }
+
---
+++ 04-Functions/Function.js
@@ -0,0 +1,12 @@
+// Function in JavaScript 
+
+        // Note: if broswer console show blue:number black:string 
+
+// DRY = Do not repeat yourself 
+function avg(a,b){
+    c= (a+b)/2;
+    return c;
+}
+c2 = avg(4,6);
+c1 = avg(14,16);
+console.log(c2,c1);
