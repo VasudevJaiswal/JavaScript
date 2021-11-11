510c51f498db2e4bf54f06ec1e244e8b85324658
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-11-11 18:00:26 +0530

    0.js

---
+++ Basic_JavaScript/03.js
@@ -0,0 +1,14 @@
+//                              Array.prototype.pop()
+/* The pop() method removes the last element from an array and returns the pop() method removes the last element from an 
+array and retruns that element . this method changes the length of the array .
+*/
+
+const plants = ['tomato','cabbage','kale','cauliflower'];
+
+console.log(plants);
+console.log(plants.pop()); // The result is - cauliflower ["Removing all the element instead of  the last element of the array"]
+console.log(plants.shift());    // The result is - Tomato ["Removing all the element instead of the first element of the array"]
+
+
+
+
